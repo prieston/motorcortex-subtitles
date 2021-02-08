80b9ea3bc5cfb5f039e17bef14605ce053f38f05
Ivan.v <1v4nvl@gmail.com>
2021-02-08 12:39:56 +0200

    export version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import SRT from "./SRT";
 
+const pkg = require("../package.json");
+
 export default {
-  npm_name: "@kissmybutton/motorcortex-subtitles",
+  npm_name: pkg.name,
+  version: pkg.version,
   incidents: [
     {
       exportable: SRT,
