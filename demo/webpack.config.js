1c51b8a35af5a8e4ab3ad92762d83574fcf87395
Alexandros Dorodoulis <aldor94@gmail.com>
2021-10-20 18:25:54 +0200

    fix: update build and demo files drop old babel config

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,71 +1,54 @@
 const path = require("path");
 const webpack = require("webpack");
 
+const dcPath = "https://code.donkeyclip.com";
+
 module.exports = {
   context: path.resolve(__dirname),
 
   entry: "./index.js",
 
-  resolve: {
-    extensions: [".js"],
-    modules: [path.resolve("./"), "node_modules"],
-  },
   output: {
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "./"),
     // the output bundle
+    filename: "./bundle.js",
+  },
 
-    path: path.resolve(__dirname, "./" /*"./dist"*/),
-  },
-  node: {
-    fs: "empty",
-  },
   module: {
     rules: [
-      {
-        test: /\.js?$/,
-        use: ["babel-loader"],
-        exclude: /node_modules/,
-      },
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "style-loader",
-            options: { sourceMap: true }, // creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader",
-            options: { sourceMap: true }, // translates CSS into CommonJS
-          },
-        ],
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
     ],
   },
 
   plugins: [
-    new webpack.ProvidePlugin({
-      Promise: "es6-promise",
-      fetch:
-        "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch",
-    }),
-
-    new webpack.HotModuleReplacementPlugin(),
     // enable HMR globally
+    new webpack.HotModuleReplacementPlugin(),
 
-    new webpack.NoEmitOnErrorsPlugin(),
     // do not emit compiled assets that include errors
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
 
   devServer: {
-    // watchContentBase: true, // initiate a page refresh if static content changes
-    host: "0.0.0.0",
+    host: "127.0.0.1",
     port: 8090,
     historyApiFallback: false,
     hot: true,
     contentBase: "./demo",
+    open: true,
+    openPage: dcPath,
+    headers: {
+      "Access-Control-Allow-Origin": dcPath,
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
   },
 };
